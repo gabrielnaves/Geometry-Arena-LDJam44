89a2b412d5000717472d3184f82854d2f962031f
Gabriel Naves da Silva <gabri.navess@gmail.com>
2019-04-29 02:36:10 -0300

    Improving particles even further, and for the last time in this project

--- Assets/Scripts/Game/LineRendererParticleSystem/ParticleSystemUtility.cs
+++ Assets/Scripts/Game/LineRendererParticleSystem/ParticleSystemUtility.cs
@@ -7,6 +7,8 @@ public class ParticleSystemUtility : MonoBehaviour {
 
     static public ParticleSystemUtility instance { get; private set; }
 
+    public AnimationCurve sizeOverLifetime;
+
     ObjectPooler pooler;
 
     void Awake() {
@@ -15,6 +17,11 @@ public class ParticleSystemUtility : MonoBehaviour {
     }
 
     public void ExplosionEffect(Vector3 position, MinMax velocityRange, int particleAmount, float lifetime, float size, Gradient gradient) {
+        ExplosionEffect(position, velocityRange, particleAmount, lifetime, size, gradient, sizeOverLifetime);
+    }
+
+    public void ExplosionEffect(Vector3 position, MinMax velocityRange, int particleAmount, float lifetime, float size, Gradient gradient, AnimationCurve sizeOverLifetime) {
+        Color color = gradient.Evaluate(Random.value);
         LineRendererParticle[] particles = new LineRendererParticle[particleAmount];
         for (int i = 0; i < particleAmount; ++i) {
             var particle = pooler.GetObject().GetComponent<LineRendererParticle>();
@@ -22,19 +29,21 @@ public class ParticleSystemUtility : MonoBehaviour {
             particle.size = size;
             particle.position = position;
             particle.velocity = Random.insideUnitCircle.normalized * velocityRange.Random();
-            particle.color = gradient.Evaluate(Random.value);
+            particle.color = color;
             particles[i] = particle;
         }
-        StartCoroutine(UpdateParticles(particles, lifetime));
+        StartCoroutine(UpdateParticles(particles, lifetime, size, sizeOverLifetime));
     }
 
-    IEnumerator UpdateParticles(LineRendererParticle[] particles, float lifetime) {
+    IEnumerator UpdateParticles(LineRendererParticle[] particles, float lifetime, float size, AnimationCurve sizeOverLifetime) {
         float timer = 0;
         while (timer < lifetime) {
             yield return null;
             timer += Time.deltaTime;
-            foreach (var particle in particles)
+            foreach (var particle in particles) {
                 particle.position += particle.velocity * Time.deltaTime;
+                particle.size = size * sizeOverLifetime.Evaluate(timer / lifetime);
+            }
         }
         for (int i = 0; i < particles.Length; ++i)
             pooler.ReturnObject(particles[i].gameObject);
